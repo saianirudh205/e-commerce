03ae7b896f43dc178fc6a0a9dbaf142b3a35f277
Vikram Jadhav <er.vikram.jadhav@gmail.com>
2024-03-03 21:11:58 +0530

    added pages

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,14 @@
 # product/urls.py
 
 from django.urls import path
-from .views import ProductListCreateAPIView, ProductRetrieveAPIView
+from .views import (
+    ProductListCreateAPIView, ProductRetrieveAPIView, ProductAddToCartListCreateAPIView,
+    ProductCartItemDetailAPIView)
+
 
 urlpatterns = [
     path('/', ProductListCreateAPIView.as_view(), name='product-list-create'),
     path('/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product-retrieve'),
+    path('/add_to_cart/', ProductAddToCartListCreateAPIView.as_view(), name='add_to_cart'),
+    path('/cart_item/<int:pk>/', ProductCartItemDetailAPIView.as_view(), name='cart_item-detail'),
 ]
